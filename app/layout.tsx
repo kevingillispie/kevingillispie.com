9c3713f65235da7f8586c8fc79dfcd9cf3f3db75
Kevin Gillispie <27305807+kevingillispie@users.noreply.github.com>
2023-12-13 12:44:50 -0800

    Re-init commit

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import Navigation from "./components/navigation/navbar"
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import './home.css'
+import Text3dWeb from "./components/text-3d-web"
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+    title: 'Kevin Gillispie, Web+Software Developer',
+    description: 'Full-stack web and software developer',
+}
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <Navigation />
+                <Text3dWeb />
+                {children}
+            </body>
+        </html>
+    )
+}
