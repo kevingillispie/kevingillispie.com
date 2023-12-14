117f2e5b7ad769dac652f57b37840cb4625a0b35
Kevin Gillispie <27305807+kevingillispie@users.noreply.github.com>
2023-12-13 23:04:42 -0800

    Update rotating-box-logo.tsx

--- app/components/rotating-box-logo.tsx
+++ app/components/rotating-box-logo.tsx
@@ -7,56 +7,56 @@ const RotatingBox: React.FC = () => {
     const containerRef = useRef<HTMLDivElement>(null);
     useEffect(() => {
         if (typeof window !== 'undefined') {
-            let camera, scene, renderer;
-			let mesh;
+            // let camera, scene, renderer;
+			// let mesh;
 
-            const onWindowResize = () => {
+            // const onWindowResize = () => {
 
-				camera.aspect = window.innerWidth / window.innerHeight;
-				camera.updateProjectionMatrix();
+			// 	camera.aspect = window.innerWidth / window.innerHeight;
+			// 	camera.updateProjectionMatrix();
 
-				renderer.setSize( window.innerWidth, window.innerHeight );
+			// 	renderer.setSize( window.innerWidth, window.innerHeight );
 
-			}
+			// }
 
-			const animate = () => {
+			// const animate = () => {
 
-				requestAnimationFrame( animate );
+			// 	requestAnimationFrame( animate );
 
-				mesh.rotation.x += 0.005;
-				mesh.rotation.y += 0.01;
+			// 	mesh.rotation.x += 0.005;
+			// 	mesh.rotation.y += 0.01;
 
-				renderer.render( scene, camera );
+			// 	renderer.render( scene, camera );
 
-			}
+			// }
 
-            const init = () => {
+            // const init = () => {
 
-				camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.1, 100 );
-				camera.position.z = 2;
+			// 	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.1, 100 );
+			// 	camera.position.z = 2;
 
-				scene = new THREE.Scene();
+			// 	scene = new THREE.Scene();
 
-				const texture = new THREE.TextureLoader().load( require('../../public/k-logo.png') );
-				texture.colorSpace = THREE.SRGBColorSpace;
+			// 	const texture = new THREE.TextureLoader().load( require('../../public/k-logo.png') );
+			// 	texture.colorSpace = THREE.SRGBColorSpace;
 
-				const geometry = new THREE.BoxGeometry();
-				const material = new THREE.MeshBasicMaterial( { map: texture } );
+			// 	const geometry = new THREE.BoxGeometry();
+			// 	const material = new THREE.MeshBasicMaterial( { map: texture } );
 
-				mesh = new THREE.Mesh( geometry, material );
-				scene.add( mesh );
+			// 	mesh = new THREE.Mesh( geometry, material );
+			// 	scene.add( mesh );
 
-				renderer = new THREE.WebGLRenderer( { antialias: true } );
-				renderer.setPixelRatio( window.devicePixelRatio );
-				renderer.setSize( window.innerWidth, window.innerHeight );
-				document.body.appendChild( renderer.domElement );
+			// 	renderer = new THREE.WebGLRenderer( { antialias: true } );
+			// 	renderer.setPixelRatio( window.devicePixelRatio );
+			// 	renderer.setSize( window.innerWidth, window.innerHeight );
+			// 	document.body.appendChild( renderer.domElement );
 
-				//
+			// 	//
 
-				window.addEventListener( 'resize', onWindowResize );
+			// 	window.addEventListener( 'resize', onWindowResize );
 
-			}
-            init()
+			// }
+            // init()
         }
     }, []);
     return <div ref={containerRef} />;
