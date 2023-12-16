9373b2fd9d8b634779d135403b075563ba9d1b90
Kevin Gillispie <27305807+kevingillispie@users.noreply.github.com>
2023-12-15 20:51:32 -0800

    Add logo data image version

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import './home.css'
-import Text3dWeb from "./components/text-3d-web"
+import RotatingBox from "./components/rotating-box-logo"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,7 +21,7 @@ export default function RootLayout({
         <html lang="en">
             <body className={inter.className}>
                 <Navigation />
-                {/* <Text3dWeb /> */}
+                <RotatingBox />
                 {children}
             </body>
         </html>
