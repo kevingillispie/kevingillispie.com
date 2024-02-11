e2106e2ddf1613ca47abb1e5c06ea5f89d564386
Kevin Gillispie <27305807+kevingillispie@users.noreply.github.com>
2024-02-11 14:03:37 -0800

    Update logo props

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,11 @@ import Klogo from './components/images/k-logo';
 
 export default function Home() {
     const [mode] = React.useState<PaletteMode>('dark');
+
+    const logoProps = {
+        w: 180,
+        h: 37
+    }
     return (
         <>
             <Script
@@ -20,7 +25,7 @@ export default function Home() {
                 </div>
                 <NavBar />
                 <div className="relative z-10 flex flex-col place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-                    <Klogo width={180} height={37} />
+                    <Klogo width={logoProps.w} height={logoProps.h} />
                     <h2 className="job-title flex flex-col mt-6 dark:drop-shadow-[0_0_0.3rem_#ffffff70]">
                         <div className="text-wrapper">
                             <div className="line line1"></div>
